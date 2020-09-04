9f330e94671d0636854bdcecce59bda338d12d5b
Jonatas Prates <jprates@dxc.com>
2020-09-04 17:11:54 -0300

    Comando calcular resto da divisão

---
+++ src/ComandoModulo.java
@@ -0,0 +1,18 @@
+
+public class ComandoModulo {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Módulo: Resto da divisão
+		 */
+		
+		double valor = 5;
+		double valor2 = 2;
+		
+		double resto = valor % valor2;
+		
+		System.out.println("Resto da divisão é: " + resto);
+	}
+
+}
