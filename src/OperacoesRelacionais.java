1d5b131593d82f4caa19ed2ed667c65403e3b3ac
Jonatas Prates <jprates@dxc.com>
2020-09-04 17:15:16 -0300

    Operadores Relacionais, igual, maior, menor, maior igual, etc

---
+++ src/OperacoesRelacionais.java
@@ -0,0 +1,25 @@
+
+public class OperacoesRelacionais {
+
+	public static void main(String[] args) {
+		
+		int nota = 20;
+		int nota2 = 10;
+		
+		if (nota == nota2) {
+			System.out.println("Igual");
+		}
+		
+		if (nota != nota2) {
+			System.out.println("Diferente");
+		}
+		
+		if (nota > nota2) {
+			System.out.println("Nota é menor que a nota 2");
+		}
+		
+		if (nota < nota2) {
+			System.out.println("Nota2 é maior que a nota");
+		}
+	}
+}
