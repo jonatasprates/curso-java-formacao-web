0fedd9c067610fa16c22919d1bfbb91a6c413292
Jonatas Prates <jprates@dxc.com>
2020-09-09 20:13:26 -0300

    criando classe aluno e executavel

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -1,5 +1,8 @@
 package cursojava.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Está é uma classe/objeto que representa o Aluno
  */
@@ -8,12 +11,14 @@ public class Aluno {
 	/*
 	 * Atributos, caracteristicas de uma classe
 	 */
-	String nome;
-	int idade;
-	String sexo;
-	String dataNascimento;
-	String rg;
-	String cpf;
+	private String nome;
+	private int idade;
+	private String sexo;
+	private String dataNascimento;
+	private String rg;
+	private String cpf;
+	
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
 	
 	/*
 	 * Construtores
@@ -29,4 +34,101 @@ public class Aluno {
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+	
+	/*
+	 * Método que retorna a média do aluno
+	 */
+	public double getMediaNota() {
+		double somaNotas = 0.0;
+		
+		for (Disciplina disciplina : disciplinas) {
+			somaNotas += disciplina.getNota();
+		}
+		return somaNotas / disciplinas.size();
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", sexo=" + sexo + ", dataNascimento=" + dataNascimento
+				+ ", rg=" + rg + ", cpf=" + cpf + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idade;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (idade != other.idade)
+			return false;
+		return true;
+	}	
+	
 }
--- src/cursojava/classes/Executavel.java
+++ src/cursojava/classes/Executavel.java
@@ -11,11 +11,58 @@ public class Executavel {
 		// new Aluno() é uma instancia (Criação de objeto)
 		// aluno1 é uma referencia para o objeto Aluno
 		Aluno aluno1 = new Aluno(); 
+		aluno1.setIdade(27);
 		
+		/*Disciplina disciplina = new Disciplina();
+		disciplina.setNota1(10);
+		disciplina.setNota2(10);
+		aluno1.setDisciplina(disciplina); */
+		
+		Disciplina disciplina = new Disciplina();
+		disciplina.setDisciplina("Informatica");
+		disciplina.setNota(10);
+		
+		Disciplina disciplina2 = new Disciplina();
+		disciplina2.setDisciplina("Banco de dados");
+		disciplina2.setNota(10);
+		
+		// Adicionando objeto na lista
+		aluno1.getDisciplinas().add(disciplina);
+		aluno1.getDisciplinas().add(disciplina2);
+		
+		
+		System.out.println("Lista de disciplinas" + aluno1.getDisciplinas().remove(0)); // remove o item da lista através do index
+		
+		System.out.println("A média do aluno é " + aluno1.getMediaNota());
+
+		
+		System.out.println(aluno1.toString()); // toString mostra a descrição do objeto na memória
 		
 		Aluno aluno2 = new Aluno("Joao");
 		
 		Aluno aluno3 = new Aluno("Carlos", 30);
+		
+		
+		//**************************************************************************************************************************************
+		
+		/*
+		 * Equals e hashCode (Diferenciar e comparar objetos)
+		 */
+		//Sem o uso do Equals e hashCode os objetos são diferentes mas contem o mesmo valor, para verificar se os objetos também são iguais
+		// é necessário criar o Equals e hashCode
+		
+		Aluno aluno4 = new Aluno();
+		aluno4.setIdade(27);
+		
+		if (aluno1.equals(aluno4)) {
+			System.out.println("Igual");
+		} else {
+			System.out.println("Diferente");
+		}
+		
+		//**************************************************************************************************************************************
+		
+		
 	}
 
 }
