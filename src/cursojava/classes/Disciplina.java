54ff0be1c6e7c8526ed2493e194ca2bdb891a604
Jonatas Prates <jprates@dxc.com>
2020-09-09 20:11:41 -0300

    Criando a classe Disciplina

---
+++ src/cursojava/classes/Disciplina.java
@@ -0,0 +1,25 @@
+package cursojava.classes;
+
+public class Disciplina {
+	
+	private double nota;
+	private String disciplina;
+	
+	public double getNota() {
+		return nota;
+	}
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+	@Override
+	public String toString() {
+		return "Disciplina [nota=" + nota + ", disciplina=" + disciplina + "]";
+	}
+	
+}
